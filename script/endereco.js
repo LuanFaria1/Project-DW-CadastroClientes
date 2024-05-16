c307d213a14a55d3fb3a55fbd4320296fc66ca15
LuanFaria1 <farialuan723@gmail.com>
2024-05-15 22:58:56 -0300

    end js

--- script/endereco.js
+++ script/endereco.js
@@ -1,22 +1,27 @@
 const url = 'https://go-wash-api.onrender.com/api/auth/address';
 
+// Função assíncrona para salvar um endereço
 async function salvarendereco() {  
+    // Obter os valores dos campos do formulário
     const titulo = document.getElementById('Titulo').value;
     const cep = document.getElementById('CEP').value;
     const endereco = document.getElementById('Endereço').value;
     const numero = document.getElementById('Número').value;
 
+    // Verificar se todos os campos obrigatórios foram preenchidos
     if (!titulo || !cep || !endereco || !numero) {
         alert("Preencha todos os campos obrigatórios");
         return;
     }
 
+    // Verificar se o CEP é válido
     const cepValido = await verificarCEP(cep);
     if (!cepValido) {
         alert("CEP inválido. Por favor, insira um CEP válido cadastrado no site ViaCEP.");
         return;
     }
 
+    // Enviar uma solicitação para salvar o endereço no servidor
     const resposta = await fetch(url, {
         method: "POST",
         body: JSON.stringify({
@@ -30,21 +35,26 @@ async function salvarendereco() {
         }
     });
 
+    // Verificar se a solicitação foi bem-sucedida
     if (resposta.ok) {
         alert("Cadastro feito com sucesso");
+        // Salvar os dados localmente
         salvarDadosLocal(titulo, cep, endereco, numero);
+        // Redirecionar para a página inicial
         window.location.href = "home.html"; 
     } else {
         alert("Ocorreu um erro ao cadastrar o endereço");
     }
 }
 
+// Função assíncrona para verificar a validade de um CEP
 async function verificarCEP(cep) {
     const viaCEPURL = `https://viacep.com.br/ws/${cep}/json/`;
     try {
         const response = await fetch(viaCEPURL);
         const data = await response.json();
 
+        // Verificar se o CEP é válido
         if (!data.erro) {
             document.getElementById('Endereço').value = data.logradouro;
             return true;
@@ -58,6 +68,7 @@ async function verificarCEP(cep) {
     }
 }
 
+// Função para salvar os dados localmente
 function salvarDadosLocal(titulo, cep, endereco, numero) {
     // Recupera os endereços salvos no localStorage
     let enderecos = JSON.parse(localStorage.getItem('enderecos')) || [];
@@ -78,7 +89,6 @@ function salvarDadosLocal(titulo, cep, endereco, numero) {
     // Salva o array de endereços atualizado no localStorage
     localStorage.setItem('enderecos', JSON.stringify(enderecos));
 }
-
 document.getElementById('CEP').addEventListener('input', function() {
     const cep = this.value;
     verificarCEP(cep);
